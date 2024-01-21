bce6787a2f526bcb5f0b8e63925db8df2e7dc063
Gbane <zeus855@msn.com>
2024-01-21 16:37:12 +0100

    Ajout de la commande pour créer un utilisateur ADMIN

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Add a short description for your command',
+)]
+class CreateAdminCommand extends Command
+{
+
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {
+        parent::__construct(null);
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::OPTIONAL, 'Ajouter email')
+            ->addArgument('password', InputArgument::OPTIONAL, 'Ajouter le mot de passe')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        $user = new User();
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setEmail($email);
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user,
+                $password
+            )
+        );
+
+        $this->em->persist($user);
+
+        $this->em->flush();
+
+        return Command::SUCCESS;
+    }
+}
